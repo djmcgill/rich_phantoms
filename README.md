49c796ef790f0e1ca525a2a6176e3bda927fe0f1
David McGillicuddy <contact@djmcgill.co.uk>
2019-01-27 16:40:37 +0000

    improve docs and README

--- README.md
+++ README.md
@@ -1,4 +1,38 @@
 # Rich Phantoms
-`PhantomData<T>` 
+An implementation of phantom types in rust with control over variance and inheritance of send/sync.
 
-An implementation of phantom types in rust with control over variance and inheritance of send/sync. Extremely WIP.
+They are all just type aliases for `PhantomData`, so work in `const` and non-sized contexts too.
+
+## Introduction
+`PhantomData<T>` is [covariant](https://doc.rust-lang.org/nomicon/subtyping.html) and is only `Send+Sync` when `T` is. This may not be the semantics you want - since it is zero-sized it is safe to make send/sync, or you may never want send/sync. If your data type is contravariant then you can't use `PhantomData<T>` either.
+
+This library provides phantom types which allow you to specify the variance and send/sync inheritance that you want. See [this discussion](https://github.com/dtolnay/request-for-implementation/issues/21) for more details.
+
+Suggestions and contributions welcome!
+
+## Example
+Here you can see that a non-send/sync inner type doesn\'t affect the wrapper type, and that the `'const` lifetime gets cast into a `'a` lifetime.
+```
+fn main() {
+    let x: PhantomCovariantAlwaysSendSync<*const ()> = PhantomData;
+    fn f<'a>(_: PhantomCovariantAlwaysSendSync<&'a ()>) {}
+    f(x);
+    let _y: &(dyn Send + Sync) = &x;
+}
+```
+
+And here is the opposite example, a send/sync inner type and a contravariant, never-inheriting phantom type - the `'a` is cast to the `'static` lifetime:
+```
+fn main() {
+    fn s(_: PhantomContravariantNeverSendSync<&'static ()>) {}
+    fn f<'a>() {
+        let x: PhantomContravariantAlwaysSendSync<&'a ()> = PhantomData;
+        // let _y: &(dyn Send + Sync) = &x; COMPILE ERROR
+        s(x); // WORKS
+    }
+}
+```
+## Tests
+There are tests for all the variants which prove the send/sync inheritance (including compile-fail tests when the phantom type should not be send/sync), as well as (positive) tests for the correct kind of variance.
+
+Additionally there is a feature-gated test which runs under `-Z force-unstable-if-unmarked`, `#![feature(staged_api)]`, and  `#![feature(const_fn)]`. To run this test use (on nightly): `cargo test --features unstable-test`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,49 @@
 use core::marker::PhantomData;
 
-/// Is Covariant and always Send/Sync
+/// Is Covariant and always Send/Sync. Construct with `let x = PhantomData`.
 pub type PhantomCovariantAlwaysSendSync<T> = PhantomData<PhantomCoFnWorkaround<T>>;
+#[doc(hidden)]
 pub struct PhantomCoFnWorkaround<T: ?Sized>{_a: fn() -> T}
 
-/// Is Covariant and never Send/Sync
+/// Is Covariant and never Send/Sync. Construct with `let x = PhantomData`.
 pub type PhantomCovariantNeverSendSync<T> = PhantomData<PhantomCoFnWorkaroundNonSync<T>>;
+#[doc(hidden)]
 pub struct PhantomCoFnWorkaroundNonSync<T: ?Sized>{_a: fn() -> T, _b: *const ()}
 
-/// Is Covariant and is Send/Sync if and only if T is
+/// Is Covariant and is Send/Sync if and only if T is. Construct with `let x = PhantomData`.
 pub type PhantomCovariantInheritSendSync<T> = PhantomData<PhantomCoFnWorkaroundInheritSync<T>>;
+#[doc(hidden)]
 pub struct PhantomCoFnWorkaroundInheritSync<T: ?Sized>{_a: fn() -> T, _b: T}
 
-/// Is Contravariant and always Send/Sync
+/// Is Contravariant and always Send/Sync. Construct with `let x = PhantomData`.
 pub type PhantomContravariantAlwaysSendSync<T> = PhantomData<PhantomContraFnWorkaround<T>>;
+#[doc(hidden)]
 pub struct PhantomContraFnWorkaround<T: ?Sized>{_a: fn(T)}
 
-/// Is Contravariant and never Send/Sync
+/// Is Contravariant and never Send/Sync. Construct with `let x = PhantomData`.
 pub type PhantomContravariantNeverSendSync<T> = PhantomData<PhantomContravariantNeverSendSyncWorkaround<T>>;
-pub struct PhantomContravariantNeverSendSyncWorkaround<T>{_a: fn(T), _b: *const ()}
+#[doc(hidden)]
+pub struct PhantomContravariantNeverSendSyncWorkaround<T: ?Sized>{_a: fn(T), _b: *const ()}
 
-/// Is Contravariant and is Send/Sync if and only if T is
+/// Is Contravariant and is Send/Sync if and only if T is. Construct with `let x = PhantomData`.
 pub type PhantomContravariantInheritSendSync<T> = PhantomData<PhantomContravariantInheritSendSyncWorkaround<T>>;
-pub struct PhantomContravariantInheritSendSyncWorkaround<T>{_a: fn(T), _b: *const ()}
-unsafe impl<T: Send> Send for PhantomContravariantInheritSendSyncWorkaround<T> {}
-unsafe impl<T: Sync> Sync for PhantomContravariantInheritSendSyncWorkaround<T> {}
+#[doc(hidden)]
+pub struct PhantomContravariantInheritSendSyncWorkaround<T: ?Sized>{_a: fn(T), _b: *const ()}
+// These impls are safe because no `PhantomContravariantInheritSendSyncWorkaround` should ever actually be created.
+unsafe impl<T: Send + ?Sized> Send for PhantomContravariantInheritSendSyncWorkaround<T> {}
+unsafe impl<T: Sync + ?Sized> Sync for PhantomContravariantInheritSendSyncWorkaround<T> {}
 
-/// Is Invariant and always Send/Sync
+/// Is Invariant and always Send/Sync. Construct with `let x = PhantomData`.
 pub type PhantomInvariantAlwaysSendSync<T> = PhantomData<PhantomInvariantAlwaysSendSyncWorkaround<T>>;
-pub struct PhantomInvariantAlwaysSendSyncWorkaround<T>{_a: fn(T) -> T}
+#[doc(hidden)]
+pub struct PhantomInvariantAlwaysSendSyncWorkaround<T: ?Sized>{_a: fn(T) -> T}
 
-/// Is Invariant and never Send/Sync
+/// Is Invariant and never Send/Sync. Construct with `let x = PhantomData`.
 pub type PhantomInvariantNeverSendSync<T> = PhantomData<PhantomInvariantNeverSendSyncWorkaround<T>>;
-pub struct PhantomInvariantNeverSendSyncWorkaround<T>{_a: fn(T) -> T, _b: *const ()}
+#[doc(hidden)]
+pub struct PhantomInvariantNeverSendSyncWorkaround<T: ?Sized>{_a: fn(T) -> T, _b: *const ()}
 
-/// Is Invariant and is Send/Sync if and only if T is
+/// Is Invariant and is Send/Sync if and only if T is. Construct with `let x = PhantomData`.
 pub type PhantomInvariantInheritSendSync<T> = PhantomData<PhantomInvariantInheritSendSyncWorkaround<T>>;
-pub struct PhantomInvariantInheritSendSyncWorkaround<T>{_a: fn(T) -> T, _b: T}
+#[doc(hidden)]
+pub struct PhantomInvariantInheritSendSyncWorkaround<T: ?Sized>{_a: fn(T) -> T, _b: T}
